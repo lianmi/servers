6d8bb5f6676c32e47736da2eaf35ce3966db5b46
lianmi <zokesky@gmail.com>
2020-11-25 22:14:49 +0800

    Update controllers.go

--- internal/app/dispatcher/controllers/controllers.go
+++ internal/app/dispatcher/controllers/controllers.go
@@ -38,11 +38,11 @@ type Login struct {
 }
 
 type LoginResp struct {
-	Username    string `form:"username" json:"username"`
+	Username    string `form:"username" json:"username"`         //注册账号
 	UserType    int    `form:"user_type" json:"user_type"`       //用户类型 1-普通，2-商户
 	State       int    `form:"state" json:"state"`               // 普通用户： 0-非VIP 1-付费用户(购买会员) 2-封号  商户：0-预审核, 1-审核通过, 2 -占位, 3-审核中
 	AuditResult string `form:"audit_result" json:"audit_result"` // 商户：  当state=3，此字段是审核的文字报告，如审核中，地址不符，照片模糊等
-	JwtToken    string `form:"jwt_token" json:"jwt_token"`
+	JwtToken    string `form:"jwt_token" json:"jwt_token"`       //令牌
 }
 
 func ParseToken(tokenSrt string, SecretKey []byte) (claims jwt.Claims, err error) {
@@ -236,7 +236,6 @@ func CreateInitControllersFn(
 				}
 				userName := claims.(jwt.MapClaims)[common.IdentityKey].(string)
 				deviceID := claims.(jwt.MapClaims)["deviceID"].(string)
-				pc.logger.Debug("LoginResponse", zap.String("userName", userName), zap.String("deviceID", deviceID), zap.String("expire", t.Format(time.RFC3339)))
 
 				//将userName, deviceID, token及expire保存到redis, 用于mqtt协议的消息的授权验证
 				pc.SaveUserToken(userName, deviceID, token, t)
@@ -251,6 +250,13 @@ func CreateInitControllersFn(
 				if int(user.User.UserType) == 2 && int(user.User.State) == 3 {
 					auditResult = "商户进驻已受理, 审核中..."
 				}
+				pc.logger.Debug("LoginResponse",
+					zap.String("userName", userName),
+					zap.String("deviceID", deviceID),
+					zap.Int("UserType", int(user.User.UserType)),
+					zap.Int("State", int(user.User.State)),
+					zap.String("expire", t.Format(time.RFC3339)),
+				)
 
 				//向客户端回复注册号及生成的JWT令牌，  用户类型，用户状态，审核结果
 				RespData(c, http.StatusOK, code, &LoginResp{
