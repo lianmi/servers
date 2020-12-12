540b0c4bac0b59139444575f5c7e13a7bede83f3
lianmi <zokesky@gmail.com>
2020-12-13 04:01:56 +0800

    dispatcher.NewApp

--- cmd/dispatcher/wire_gen.go
+++ cmd/dispatcher/wire_gen.go
@@ -103,30 +103,46 @@ func CreateApp(cf string) (*app.Application, error) {
 	mqttOptions, err := mqtt.NewMQTTOptions(viper)
 	if err != nil {
 		//TODO
-		logger.Error(" mqtt.NewMQTTOptions", zap.Error(err))
+		logger.Error("mqtt.NewMQTTOptions", zap.Error(err))
 		return nil, err
 	} else {
-		logger.Debug("NewMQTTOptions succeed")
+		logger.Debug("mqtt.NewMQTTOptions succeed")
 	}
 	mqttClient := mqtt.NewMQTTClient(mqttOptions, pool, nsqMqttChannel, logger)
 	httpOptions, err := http.NewOptions(viper)
 	if err != nil {
+		//TODO
+		logger.Error("http.NewOptions", zap.Error(err))
 		return nil, err
+	} else {
+		logger.Debug("http.NewOptions succeed")
 	}
 	lianmiApisController := controllers.NewLianmiApisController(logger, lianmiApisService)
 	initControllers := controllers.CreateInitControllersFn(lianmiApisController)
 	engine := http.NewRouter(httpOptions, logger, initControllers, tracer)
 	apiClient, err := consul.New(consulOptions)
 	if err != nil {
+		//TODO
+		logger.Error("consul.New", zap.Error(err))
 		return nil, err
+	} else {
+		logger.Debug("consul.New succeed")
 	}
 	server, err := http.New(httpOptions, logger, engine, apiClient)
 	if err != nil {
+		//TODO
+		logger.Error("http.New", zap.Error(err))
 		return nil, err
+	} else {
+		logger.Debug("http.New succeed")
 	}
 	application, err := dispatcher.NewApp(dispatcherOptions, logger, nsqClient, mqttClient, server, nsqChannel)
 	if err != nil {
+		//TODO
+		logger.Error("dispatcher.NewApp", zap.Error(err))
 		return nil, err
+	} else {
+		logger.Debug("dispatcher.NewApp")
 	}
 	return application, nil
 }
