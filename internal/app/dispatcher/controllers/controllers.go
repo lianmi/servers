8d101d185514b8d3010f9f7f2488eb792a4cc073
lianmi <zokesky@gmail.com>
2020-12-13 16:21:02 +0800

    Update controllers.go

--- internal/app/dispatcher/controllers/controllers.go
+++ internal/app/dispatcher/controllers/controllers.go
@@ -394,7 +394,7 @@ func CreateInitControllersFn(
 		orderGroup.Use(authMiddleware.MiddlewareFunc())
 		{
 			//商户端: 将完成订单拍照所有图片上链
-			// orderGroup.POST("/uploadorderimages", pc.UploadOrderImages)
+			orderGroup.POST("/uploadorderimages", pc.UploadOrderImages)
 
 			//用户端: 根据 OrderID 获取所有订单拍照图片
 			// orderGroup.GET("/orderimages:/orderid", pc.DownloadOrderImages)
