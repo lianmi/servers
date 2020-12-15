b6e1eb1129d01f7de6a5a03deb0c6719fa82c33f
lianmi <zokesky@gmail.com>
2020-12-15 10:26:11 +0800

    Update controllers.go

--- internal/app/dispatcher/controllers/controllers.go
+++ internal/app/dispatcher/controllers/controllers.go
@@ -379,7 +379,7 @@ func CreateInitControllersFn(
 			productGroup.GET("/generalproduct/:productid", pc.GetGeneralProductByID)
 
 			//查询通用商品分页-按商品种类查询
-			productGroup.POST("/generalproducts/list", pc.GetGeneralProductPage)
+			productGroup.POST("/generalproductslist", pc.GetGeneralProductPage)
 
 			//根据商户注册号查询所有上架商品
 			productGroup.POST("/productslist", pc.GetProductsList)
