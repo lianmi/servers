308d1c681b471ce5281d4953a96b714aec15c5ea
lianmi <zokesky@gmail.com>
2020-12-24 14:47:43 +0800

    vip

--- internal/app/orderservice/repositories/order.go
+++ internal/app/orderservice/repositories/order.go
@@ -14,6 +14,7 @@ type OrderRepository interface {
 	UpdateProduct(product *models.Product) error
 	DeleteProduct(productID, username string) error
 	AddPreKeys(prekeys []*models.Prekey) error
+	GetVipUserPrice(payType int) (*models.VipPrice, error)
 }
 
 type MysqlOrderRepository struct {
