a1380357bb48cb0af35f6906578eb74a13ab2e5e
wujehy <wujehy@139.com>
2021-04-15 11:01:50 +0800

    Update StoreProductItems ProductId 5

--- internal/pkg/models/store_products.go
+++ internal/pkg/models/store_products.go
@@ -11,9 +11,9 @@ type StoreProductItems struct {
 	CreatedAt   time.Time      `json:"-"`
 	UpdatedAt   time.Time      `json:"-"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
-	StoreUUID   string         `json:"store_uuid"`  // 店铺的uuid
-	ProductId   string         `json:"product_id"`  // 支持的彩种id
-	UserFormat  string         `json:"user_format"` // 用户定义的展示格式 自定义
-	UserData    string         `json:"user_data"`   // 用户定义的json 这个商品的特殊定义 , 用户可以自定义描述 , 将以是json
+	StoreUUID   string         `json:"store_uuid"`                                                                     // 店铺的uuid
+	ProductId   string         `json:"product_id" gorm:"column:product_id;comment:商品UUID;type:varchar(191);size:191;"` // 支持的彩种id
+	UserFormat  string         `json:"user_format"`                                                                    // 用户定义的展示格式 自定义
+	UserData    string         `json:"user_data"`                                                                      // 用户定义的json 这个商品的特殊定义 , 用户可以自定义描述 , 将以是json
 	ProductInfo GeneralProduct `json:"-" gorm:"foreignKey:ProductId;references:ProductId"`
 }
