37f793a14af7019ad4b2d946bc858fb15434e42e
wujehy <wujehy@139.com>
2021-04-18 03:29:01 +0800

    update  OrderPayToBusiness add store check

--- internal/app/dispatcher/controllers/order.go
+++ internal/app/dispatcher/controllers/order.go
@@ -175,7 +175,7 @@ func (pc *LianmiApisController) OrderPayToBusiness(context *gin.Context) {
 		return
 	}
 
-	if getStoreInfo.StoreType.String() == "" {
+	if getStoreInfo.GetStoreType() == global.StoreType_ST_Undefined {
 		RespFail(context, http.StatusNotFound, codes.InvalidParams, "商户信息未定义商店类型")
 		return
 	}
