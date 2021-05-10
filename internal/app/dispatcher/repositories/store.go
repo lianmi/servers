df3bcc8de7546ddbb2ab3a17e30d1489c3ff1bca
lianmi <zokesky@gmail.com>
2021-05-10 21:31:45 +0800

    搜索

--- internal/app/dispatcher/repositories/store.go
+++ internal/app/dispatcher/repositories/store.go
@@ -309,8 +309,7 @@ func (s *MysqlLianmiRepository) GetStores(req *Order.QueryStoresNearbyReq) (*Ord
 	if req.Address != "" {
 		s.logger.Debug(req.Address)
 		wheres = append(wheres, []interface{}{"address like ? ", "%" + req.Address + "%"})
-	}
-	if req.Keys != "" {
+	} else if req.Keys != "" {
 		s.logger.Debug(req.Keys)
 		wheres = append(wheres, []interface{}{"keys like ? ", "%" + req.Keys + "%"})
 	}
